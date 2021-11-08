faa1cac168797c3d7db9fbc064845a8d7016967b
Kaitlyn Marshall <pixiwyn@Tataru-3.local>
2021-11-08 11:34:28 -0700

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <h1>Webpack Starter Template</h1>
+      );
+} 
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
